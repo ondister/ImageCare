9f3eec17b931e064c266f484f2e5b80b4599c60e
alex <ondister@gmail.com>
2025-12-19 10:07:22 +0300

    Remove modules from disk

--- Modules/ImageCare.Modules.Logging/ViewModels/ErrorLogMessageViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ImageCare.Modules.Logging.ViewModels;
-
-internal sealed class ErrorLogMessageViewModel : LogMessageViewModel
-{
-	/// <inheritdoc />
-	public ErrorLogMessageViewModel(DateTimeOffset timestamp, string message, string? exceptionMessage)
-		: base(timestamp, message, exceptionMessage) { }
-}
--- Modules/ImageCare.Modules.Logging/ViewModels/WarningLogMessageViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ImageCare.Modules.Logging.ViewModels;
-
-internal sealed class WarningLogMessageViewModel : LogMessageViewModel
-{
-	/// <inheritdoc />
-	public WarningLogMessageViewModel(DateTimeOffset timestamp, string message, string? exceptionMessage)
-		: base(timestamp, message, exceptionMessage) { }
-}
